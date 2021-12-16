218b5c1e79d45839a5a889488c10189627fdc174
jinsung <jsung1103@gmail.com>
2021-12-16 13:56:11 +0900

    first commit

---
+++ exerciseC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exerciseC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
